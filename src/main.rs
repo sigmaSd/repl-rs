c0d13e6314ff516f1239cb52f88f834a77bd9324
Nbiba Bedis <bedisnbiba@gmail.com>
2019-01-27 19:53:19 +0100

    cmds

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,23 @@ mod history;
 mod repl;
 mod terminal;
 
+use std::env;
 use terminal::Terminal;
 
 fn main() {
+    let arg = env::args().nth(1).unwrap_or_else(|| "".to_string());
+    match arg.as_str() {
+        "" => {}
+        "-h" | "--help" => {
+            println!(" Rust repl!");
+            std::process::exit(0);
+        }
+        "-v" | "--version" => {
+            println!(" Rust Repl version 0.1.0");
+            std::process::exit(0);
+        }
+        _ => {}
+    }
     let mut terminal = Terminal::new();
     terminal.run();
 }
